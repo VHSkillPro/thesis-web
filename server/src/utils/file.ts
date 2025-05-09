c6f49154598fd69318e6a9e723055d48a176f233
Hazzu <hailaconsutu558@gmail.com>
2025-05-09 22:50:00 +0700

    fix: rethrow HttpException in service methods to improve error handling

--- server/src/utils/file.ts
+++ server/src/utils/file.ts
@@ -1,6 +1,11 @@
-import { unlink } from 'fs';
+import { unlink, existsSync } from 'fs';
 
 export const removeFile = (filePath: string) => {
+  if (!existsSync(filePath)) {
+    console.error('File does not exist:', filePath);
+    return;
+  }
+
   unlink(filePath, (err) => {
     if (err) {
       console.error('Error deleting file:', err);
